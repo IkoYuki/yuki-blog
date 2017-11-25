38b35ce4fb5226dacea57336c7794c8276944fd8
Yuki <ikomayuki@gmail.comikoyuki4:~/workspace/171116albumapp (master) $  ikoyuki4:~/workspace/171116albumapp (master) $   git config --global user.email ikomayuki@gmail.com>
2017-11-25 06:18:42 +0000

    initial message

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+  let postsUrl = 'https://raw.githubusercontent.com/babelthuap/hayalab-blog/master/posts.json';
+  let $postsDiv = $('#posts');
+
+  $.get(postsUrl)
+      .done(function(data) {
+        console.log('data:', data);
+        let posts = JSON.parse(data);
+        for (let post of posts) {
+          let $post = $('<div>');
+          
+          let $title = $('<h3>');
+          $title.text(post.title);
+          $post.append($title);
+
+          let $content = $('<p>');
+          $content.text(post.content);
+          $post.append($content);
+
+          $postsDiv.append($post);
+        }
+      })
+      .fail(function(err) {
+        console.log('There was an error fetching posts:', err);
+      });
+
+});
